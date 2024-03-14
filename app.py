29c5665d2660b3b86be4c62523785070fbb2de89
porottir <riccardo.porotti@prometeia.com>
2024-03-14 14:43:55 +0100

    Add README.md

--- app.py
+++ app.py
@@ -32,15 +32,17 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-@st.experimental_singleton
+@st.cache_resource
 def get_driver():
     options = Options()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-
-
+    options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
+    return webdriver.Chrome(
+        service=Service(
+            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        ),
+        options=options,
+    )
 
 def solve_captcha():
     captcha = AmazonCaptcha.fromdriver(driver)
