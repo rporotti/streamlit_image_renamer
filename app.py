4ce814fa1e8ac7f8b34ae8e0837c65711cf890b3
porottir <riccardo.porotti@prometeia.com>
2024-03-14 14:44:46 +0100

    Add README.md

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 
 @st.cache_resource
 def get_driver():
