e91b4f4280a87c07c276f520e0bef8ab8ef995d1
porottir <riccardo.porotti@prometeia.com>
2024-04-12 13:59:46 +0200

    Handles more than 10 images, do not show other ASIN

--- app.py
+++ app.py
@@ -15,28 +15,34 @@ files = {}
 if option == "From upload":
     uploaded_files = st.file_uploader("Choose a file", accept_multiple_files=True)
     files = load_images_uploaded(uploaded_files)
-    sortable = True
+    asin = None
 elif option == "From URL":
     url_page = st.text_input("Insert URL")
     download = False
+    asin = url_page.split("/")[-1]
     if st.button("Start"):
         if not os.path.exists("output"):
             os.makedirs("output")
         if not os.path.exists("to_download"):
             os.makedirs("to_download")
-        asin = url_page.split("/")[-1]
 
-        if not any([asin in x for x in os.listdir("output")]):
-            title = download_images_from_url(url_page=url_page)
-            download = True
-            st.markdown(title)
+
+        if not os.path.exists(f"output/{asin}"):
+            os.makedirs(f"output/{asin}")
+            if not any([asin in x for x in os.listdir(f"output/{asin}")]):
+                title = download_images_from_url(url_page=url_page)
+                download = True
+                st.markdown(title)
 
     cols = st.columns(3)
-    uploaded_files = sorted(os.listdir("output"))
-    files = load_images(uploaded_files)
-    sortable = True
 
-if files:
+if (option == "From upload" and files) or (option == "From URL" and os.path.exists(f"output/{asin}") and url_page!=""):
+    if option == "From URL":
+
+        if os.path.exists(f"output/{asin}"):
+
+            uploaded_files = sorted(os.listdir(f"output/{asin}"))
+            files = load_images(uploaded_files, asin)
     cols = st.columns(2)
     with cols[0]:
         sel = sort_items(list(files.keys()), direction="vertical")
@@ -54,17 +60,25 @@ if files:
 
     asin = st.text_input("ASIN")
     if st.button("Submit"):
+        if not os.path.exists(f"to_download/{asin}"):
+            os.makedirs(f"to_download/{asin}")
         images = []
         for index, key in enumerate(sel):
             ext = key.split(".")[-1]
             image = files[key][0]
             prfix = f'prefix_{key}'
-            if "PT" in prfix:
-                filename = f"to_download/{asin}.PT{str(index).zfill(2)}.{ext}"
+
+            suffix = (f"PT{str(index).zfill(2)}" if "PT" in st.session_state[prfix] else st.session_state[prfix])
+            filename = f"to_download/{asin}/{asin}.{suffix}.{ext}"
+
+            if image.mode == "JPEG":
+                image.save(filename.format(im_format=image.format))
+                # in most case, resulting jpg file is resized small one
             else:
-                filename = f"to_download/{asin}.{st.session_state[prfix]}.{ext}"
-            print(filename)
-            image.save(filename.format(im_format=image.format))
+                rgb_im = image.convert("RGB")
+                rgb_im.save(filename.format(im_format=image.format))
+                # some minor case, resulting jpg file is larger one, should meet your expectation
+
             images.append(filename)
         download_multiple_files(images)
 
